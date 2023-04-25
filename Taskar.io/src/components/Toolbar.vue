8c462cbd17fbe93db96790db0e4d3b1370f3a7b1
mili-ae <gyndenovaa@gmail.com>
2023-04-25 21:29:44 +0300

    Добавить анимации при наведении на некоторые кнопки

--- Taskar.io/src/components/Toolbar.vue
+++ Taskar.io/src/components/Toolbar.vue
@@ -7,17 +7,21 @@
             <button href="" class="btn" title="Дать сотруднику задание">
                 <v-icon name= "bi-person-plus-fill" scale="1.3"/>
             </button>
-            <button href="" class="btn" title="Выбрать несколько заданий">
+            <button href="" class="btn" title="Выбрать несколько заданий" onclick="Test">
                 <v-icon name= "ri-checkbox-multiple-fill" scale="1.3"/>
             </button>
         </div>
     </div>
 
+    <div class="app-name">
+        <h1>Taskar.io</h1>
+    </div>
+
     <div class="account">
         <div class="user">
-            <p class="username">John Doe</p>
+            <p class="username">mili-ae</p>
             <button href="" class="btn" scale="2">
-                <img src="" alt="avatar">
+                <img src="../images/37480555.jpg" alt="avatar">
             </button>
         </div>
     </div>
@@ -26,10 +30,32 @@
 <style scoped>
     button {
         background-color: transparent;
+        border: 1px solid white;
         color: white;
         border-radius: 50%;
         width: 3em;
         height: 3em;
+        transition: color 50ms ease-in;
+        transition: border-color 50ms ease-in;
+    }
+
+    p, h1 {
+        -moz-user-select: none;
+        -khtml-user-select: none;
+        -webkit-user-select: none;
+        -ms-user-select: none;
+        user-select: none;
+    }
+
+    h1 {
+        font-weight: bold;
+        text-transform: uppercase;
+    }
+
+    button > img {
+        width: 100%;
+        height: 100%;
+        border-radius: 50%;
     }
 
     .toolbar,
@@ -47,7 +73,6 @@
 
     .account {
         justify-content: right;
-        
     }
 
     .user {
@@ -60,4 +85,10 @@
         margin-right: 10px;
         font-size: 20px;
     }
+
+    button:hover {
+        background-color: white;
+        color: black
+    }
+
 </style>
